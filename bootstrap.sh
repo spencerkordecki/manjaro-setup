410ed3feb8f1b7c58ec8470d4e47168657656cb4
Spencer <spencerkordecki@gmail.com>
2019-09-24 11:22:52 -0400

    copy over settings from config directory when setting up applications

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,10 +1,11 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
 # Global Variables
 GIT_USER_EMAIL="spencerkordecki@gmail.com"
 GIT_USER_NAME="Spencer Kordecki"
 YAY_GIT_REPO="https://aur.archlinux.org/yay.git"
 SNAP_GIT_REPO="https://aur.archlinux.org/snapd.git"
+CONFIG_DIRECTORY=~/.config/
 USER_APPLICATIONS=(
     telegram-desktop-bin
     visual-studio-code-bin
@@ -27,6 +28,12 @@ sudo pacman --needed --noconfirm -S git zsh
 printf "Configuring 'git'...\n"
 git config --global user.email $GIT_USER_EMAIL
 git config --global user.name $GIT_USER_NAME
+
+printf "Changing Default Shell to 'zsh'...\n"
+chsh -s /usr/bin/zsh
+
+printf "Installing 'oh-my-zsh'...\n"
+yay --needed --noconfirm -S oh-my-zsh-git
 
 printf "Installing 'yay'...\n"
 git clone $YAY_GIT_REPO /tmp/yay
@@ -53,7 +60,6 @@ do
     yay --needed --noconfirm -S "$i"
 done
 
-
 printf "Installing 'snap' Applicatons...\n"
 for i in "${SNAP_APPLICATIONS[@]}"
 do
@@ -61,7 +67,29 @@ do
     snap install "$i"
 done
 
-printf "Cleaning Cached Packages"
+printf "Copying Files from config/ to ~/.config/...\n"
+shopt -s dotglob
+for f in $(find config/* -type f)
+do
+    file=$(basename "$f")
+    fileDirectory=$(dirname "$f")
+    targetFile=${CONFIG_DIRECTORY}${f}
+    targetDirectory=${CONFIG_DIRECTORY}${fileDirectory}
+    if [ ! -e $targetFile ]
+    then
+        if [ ! -d $targetDirectory ]
+        then
+            mkdir -p $targetDirectory
+        fi
+        touch $targetFile
+    fi
+    
+    # Use /bin/cp to prevent removing the 'cp -i alias' from bashrc
+    printf "Copying $f...\n"
+    /bin/cp -R $f $targetFile
+done
+
+printf "Cleaning Cached Packages...\n"
 yay --noconfirm -Sc
 
 printf "Removing /tmp directory...\n"
